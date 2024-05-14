5007c1262b4070b3b54e822acb215a34141d272e
Sasidhar <Sasidhar1826@gmail.com>
2024-05-14 19:36:55 +0530

    preapred app for deployment

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ import { showAlert } from './alert';
 
 export const login = async (email, password) => {
   try {
-    const res = await axios.post('http://127.0.0.1:3000/api/v1/users/login', {
+    const res = await axios.post('/api/v1/users/login', {
       email,
       password,
     });
@@ -20,7 +20,7 @@ export const login = async (email, password) => {
 
 export const logout = async () => {
   try {
-    const res = await axios.get('http://127.0.0.1:3000/api/v1/users/logout');
+    const res = await axios.get('/api/v1/users/logout');
     if (res.data.status === 'success') location.reload(true);
   } catch {
     showAlert('error', 'Error loggin out!');
