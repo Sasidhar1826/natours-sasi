03c4125ee55d56904596c0d1fa699c104921ac47
Sasidhar <Sasidhar1826@gmail.com>
2024-05-15 16:05:07 +0530

    course completed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,9 +59,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   try {
     const tour = session.client_reference_id;
-    const user = await User.findOne({ email: session.email })._id;
+    const user = await User.findOne({ email: session.customer_details.email })
+      ._id;
     const price = session.amount_total / 100;
-    console.log(user);
     await Booking.create({ tour, user, price });
   } catch (error) {
     console.log(error);
